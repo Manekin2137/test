615034a5f5c0f2278791eb98d2f1626884131627
Manekin2137 <bartosz.strzalkowski06@wp.pl>
2023-09-12 13:20:27 +0200

    Dodanie liczba Holder

---
+++ projekt1/nazwa/src/com/company/LiczbaHolder.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class LiczbaHolder {
+    int a = 5;
+
+
+    public LiczbaHolder(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
